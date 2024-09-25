8355510cf81af0cfebdc64535a7040c6b0abb5df
Hernan <hernanariza69@gmail.com>
2024-09-25 16:40:59 -0500

    feat: :sparkles:

--- src/main/java/com/GestionInventario/v1/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GestionInventario.v1.repository;
-
-import com.GestionInventario.v1.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
